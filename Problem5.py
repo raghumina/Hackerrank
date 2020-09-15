b0706e55578a785b07e6f0477c5362b276f198f4
raghu <raghumina11@gmail.com>
2020-09-15 18:27:53 +0530

    Update Problem5.py

--- Problem5.py
+++ Problem5.py
@@ -54,3 +54,19 @@
 # the instructions above and reading input from stdin.
 
 # lets Start
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+i1 = int(input())
+d2 = float(input())
+s2 = str(input())
+# Read and save an integer, double, and String to your variables.
+print(i + i1)
+# Print the sum of both integer variables on a new line.
+print(d + d2)
+# Print the sum of the double variables on a new line.
+print("HackerRank " + s2)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
